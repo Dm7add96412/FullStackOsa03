9a56b11286533c48e459bbf912cc0312cac55be9
Dm7add9 <juha.haipus@gmail.com>
2022-12-29 00:09:40 +0200

    part03

--- index.js
+++ index.js
@@ -100,6 +100,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
   })
 
-const PORT = 3001
-app.listen(PORT)
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
+})
