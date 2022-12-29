5ce28f575947131aff4a90812eda586e6d8aa8e8
Dm7add9 <juha.haipus@gmail.com>
2022-12-29 12:22:01 +0200

    part3b done

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('post-body', function (req, res) { 
     return JSON.stringify({Name: req.body.name,
